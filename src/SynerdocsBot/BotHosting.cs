8a4d68ca675b101c889734b9d4596e0931a3b5cc
ebugusey <ebugusey@users.noreply.github.com>
2021-03-01 22:58:58 +0400

    feat: Добавлено логирование ошибок команд
    По умолчанию CommandsNext не логирует ошибки, возникающие при выполнении
    команд, не смотря на то, что логирование в самом клиенте настроено
    и этот модуль имеет доступ к логеру.

--- src/SynerdocsBot/BotHosting.cs
+++ src/SynerdocsBot/BotHosting.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace SynerdocsBot
 {
@@ -11,13 +12,16 @@ namespace SynerdocsBot
     {
         readonly DiscordClient _botClient;
         readonly IServiceProvider _serviceProvider;
+        readonly ILogger<BotHosting> _logger;
 
         public BotHosting(
             DiscordClient botClient,
-            IServiceProvider serviceProvider)
+            IServiceProvider serviceProvider,
+            ILogger<BotHosting> logger)
         {
             _botClient = botClient;
             _serviceProvider = serviceProvider;
+            _logger = logger;
         }
 
         public async Task StartAsync(CancellationToken cancellationToken)
@@ -28,6 +32,8 @@ namespace SynerdocsBot
                 Services = _serviceProvider,
             });
 
+            commands.CommandErrored += LogCommandError;
+
             commands.RegisterCommands<CommandsModule>();
 
             await _botClient.ConnectAsync();
@@ -36,6 +42,15 @@ namespace SynerdocsBot
         public async Task StopAsync(CancellationToken cancellationToken)
         {
             await _botClient.DisconnectAsync();
+        }
+
+        Task LogCommandError(CommandsNextExtension sender, CommandErrorEventArgs e)
+        {
+            _logger.LogError(e.Exception,
+                "{Command} thrown an exception",
+                e.Command);
+
+            return Task.CompletedTask;
         }
     }
 }
